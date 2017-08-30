2ff90b2c54d8fff6fcde37b3e7eef9bdd36526e7
JY <kjy0939@gmail.com>
2017-08-30 20:15:48 +0900

    라우팅

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,38 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+//Route::get('/', function () {
+//    return view('welcome');
+//});
 
-Route::get('/', function () {
-    return view('welcome');
+//Route::get('/', function () {
+//    return '<h1>Hello Foo</h1>';
+//});
+
+//Route::get('/{foo}', function ($foo) {
+//    return $foo;
+//});
+
+//Route::get('/{foo?}', function ($foo = 'bar') {
+//    return $foo;
+//});
+
+//Route::pattern('foo', '[0-9a-zA-Z]{3}');
+
+//Route::get('/{foo?}', function ($foo = 'bar') {
+//    return $foo;
+//});
+
+//Route::get('/{foo?}', function ($foo = 'bar') {
+//    return $foo;
+//})->where('foo', '[0-9a-zA-Z]{3}');
+
+Route::get('/', [
+    'as' => 'home',
+    function () {
+        return '제 이름은 "home" 입니다.';
+    }
+]);
+
+Route::get('/home', function () {
+    return redirect(route('home'));
 });
