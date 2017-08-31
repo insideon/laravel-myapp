81e890932d736579c19848fa1ec9306b672b43e0
JY <kjy0939@gmail.com>
2017-08-31 11:46:22 +0900

    데이터베이스 시딩

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -22,3 +22,13 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
         'remember_token' => str_random(10),
     ];
 });
+
+$factory->define(App\Article::class, function (Faker\Generator $faker) {
+    $date = $faker->dateTimeThisMonth;
+    return [
+        'title' => $faker->sentence(),
+        'content' => $faker->paragraph(),
+        'created_at' => $date,
+        'updated_at' => $date,
+    ];
+});
